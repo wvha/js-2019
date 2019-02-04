371c674a3f237d1fed48d2424f23d8d8901555d3
wvha <Wvha17@gmail.com>
2019-02-04 12:05:45 -0800

    first commit 929-unique email addresses

---
+++ leetcode/easy/929-unique-email-addresses.js
@@ -0,0 +1,44 @@
+/*
+https://leetcode.com/problems/unique-email-addresses/
+emails have local name and domain name, separated by @ sign
+emails with '.' will be forwarded to same address without dots
+emails with '+', everything after first plus sign will be ignored
+
+how many unique email addresses do we have?
+
+11:35am
+
+- go through each email
+- use a set to check for unique
+- have a count?
+- simplify email to basic local and domain name
+
+inside the algo...
+- split by the @
+- split by the +
+- in the +[0], remove the dots
+- join the result and domain name
+- add this to the set
+
+*/
+
+var numUniqueEmails = function(emails) {
+  let set = new Set();
+  let count = 0;
+  emails.forEach(function(email) {
+    let address = email.split('@');
+    let plus = address[0].split('+');
+    let output = plus[0].split('.').join('') + '@' + address[1];
+    // console.log(output)
+    if (!set.has(output)) {
+      set.add(output);
+      count++;
+    }
+  })
+  return count;
+}
+
+let emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+
+console.log(numUniqueEmails(emails)) // 2
+
